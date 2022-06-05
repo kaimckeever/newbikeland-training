310a03f8acadd1e63e12a81a0fdab7211d0026be
Kai McKeever <16709970+kaimckeever@users.noreply.github.com>
2022-06-05 10:32:30 -0400

    add decorator to index and use CORS defaults

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app)
 Talisman(app)
 csrf = SeaSurf(app)
 app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
 
 @app.route("/")
+@cross_origin()
 def index():
     return render_template("index.html", strava_auth="")
